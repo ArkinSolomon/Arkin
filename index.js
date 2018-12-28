d539716c0ec6ba52fbfb2ec4bf652b979d16e443
Arkin Solomon <arkinsolomon@gmail.com>
2018-12-28 15:11:14 -0800

    Basic functions
    - Added basic functions

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-exports.log = function(msg){
-  console.log(msg);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+exports.sleep = function(delay){
+  var start = new Date().getTime();
+  while(new Date().getTime()<start+delay);
+}
+
+exports.end = function(){
+  process.exit(0);
+}
+
+exports.error = function(ERR){
+  console.log("There was an error:\n"+ ERR);
+  process.exit(1);
 }
