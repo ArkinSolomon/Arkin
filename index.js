4f57109629931f50a3051f2c1c238d75a5e05e85
Arkin Solomon <arkinsolomon@gmail.com>
2018-12-30 12:30:39 -0800

    Testing

--- index.js
+++ index.js
@@ -54,15 +54,15 @@ exports.getDate = function(config){
   var yyyy = today.getFullYear();
 
   if(dd < 10) {
-      dd = '0' + dd
+      dd = '0' + dd;
   }
 
   if(mm < 10) {
-      mm = '0'+mm
+      mm = '0'+mm;
   }
 
   if (params.separator === undefined){
-    sep = '/'
+    sep = '/';
   }
 
   var sep = params.separator;
